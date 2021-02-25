edf28e14d219f20afadd19fde86f713e37b3b33d
Shauna Gordon-McKeon <shaunagm@gmail.com>
2021-02-25 17:33:43 -0500

    Improve list views, flesh out resource detail view

--- python-in-edu/resources/urls.py
+++ python-in-edu/resources/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('profiles', views.ProfileListView.as_view(), name='profile_list'),
-    path('profile/<slug:username>', views.ProfileDetailView.as_view(), name='profile_detail'),
+    path('profile/<str:username>', views.ProfileDetailView.as_view(), name='profile_detail'),
     path('resource', views.ResourceListView.as_view(), name='resource_list'),
     # FIXME: below should probably be a slug
-    path('resources/<int:resource>', views.ResourceDetailView.as_view(), name='resource_detail'),
+    path('resources/<int:pk>', views.ResourceDetailView.as_view(), name='resource_detail'),
 ]
--- python-in-edu/resources/views.py
+++ python-in-edu/resources/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
-
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 from .models import Profile, Resource
 
@@ -17,6 +18,11 @@ class ResourceListView(generic.ListView):
 class ProfileDetailView(generic.DetailView):
     model = Profile
     template_name = 'resources/profile_detail.html'
+
+    def get_object(self, queryset=None):
+        username = self.kwargs.get('username', None)
+        user = get_object_or_404(User, username=username)
+        return user.profile
 
 
 class ProfileListView(generic.ListView):
