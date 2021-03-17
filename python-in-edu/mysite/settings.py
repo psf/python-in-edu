b5cd06a2af52c3669ca57f0bc9990ee885dc772a
Shauna Gordon-McKeon <shaunagm@gmail.com>
2021-03-17 14:42:49 -0400

    fix static

--- python-in-edu/mysite/settings.py
+++ python-in-edu/mysite/settings.py
@@ -197,12 +197,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_build')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static_source'),
 )
 
 
