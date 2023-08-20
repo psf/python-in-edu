91bbcd60f42420c54eddda943dfc82f54d9b6975
Nandika-A <nandikaagrawal610@gmail.com>
2023-08-20 14:50:56 +0530

    crispy forms added

--- python-in-edu/mysite/settings.py
+++ python-in-edu/mysite/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'multiselectfield',
     'crispy_forms',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
